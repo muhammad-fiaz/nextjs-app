8d37af2ddf5df541a52c144bc42da8e3f4a9d4a1
Muhammad Fiaz <s.muhammadfiaz2003@gmail.com>
2023-10-18 22:18:05 +0530

    updated

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 
 const { execSync } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
 const runCommand = command => {
   try {
@@ -13,9 +14,16 @@ const runCommand = command => {
 };
 
 const repoName = process.argv[2] || 'my-nextjs-app'; // Default to a name if not provided
-const templateRepoURL = 'https://github.com/muhammadfiaz/nextjs-app';
+const templateRepoURL = 'https://github.com/muhammad-fiaz/nextjs-app';
 
 const projectDir = path.resolve(process.cwd(), repoName);
+
+// Check if the directory already exists
+if (fs.existsSync(projectDir)) {
+  console.error(`Error: Directory '${repoName}' already exists.`);
+  console.error('Choose a different project name or delete the existing directory.');
+  process.exit(1);
+}
 
 const gitCheckoutCommand = `git clone --depth 1 ${templateRepoURL} ${projectDir}`;
 const installDepsCommand = `cd ${projectDir} && npm install`;
